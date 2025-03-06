276a63f137cb24e0a8202251fc68f94fcdfaac44
Akin Holo <holoakinseloyin@gmail.com>
2025-03-06 04:16:12 +0100

    Add remote origin
    Linked local origin to remote

---
+++ reusesable-component/src/App.jsx
@@ -0,0 +1,14 @@
+
+import Nav from './nav.jsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Nav>hello</Nav> 
+    </>
+  )
+}
+
+export default App
---
+++ reusesable-component/src/nav.jsx
@@ -0,0 +1,11 @@
+
+function Nav() {
+   return (
+      <div>
+         <h1>Week 8 Deliverable</h1>
+         <h2>Component Name</h2>
+      </div>
+   )
+}
+
+export default Nav
